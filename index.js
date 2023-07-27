82887b7393defef6b7b02b59602db60b7f7f1125
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-07-27 00:28:38 -0600

    Serving static site in public folder

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const express = require('express')
 // Create express server
 const app = express()
 
+// Serve static site in '/'
+app.use(express.static('public'))
+
 // Routes
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   console.log('/ required')
 
   res.json({
     ok: true,
   })
-})
+}) */
 
 // Listen to requests on port
 app.listen(4000, () => {
