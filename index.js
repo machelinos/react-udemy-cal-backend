a615087eb2a9ac048d6ecc8ff91501c035800802
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-07-27 00:18:32 -0600

    Installing and setting up express - First route

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello world!')
+const express = require('express')
+
+// Create express server
+const app = express()
+
+// Routes
+app.get('/', (req, res) => {
+  console.log('/ required')
+
+  res.json({
+    ok: true,
+  })
+})
+
+// Listen to requests on port
+app.listen(4000, () => {
+  console.log(`Server running in port: ${4000}`)
+})
